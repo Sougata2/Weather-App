d497ff9eb2060355bd892d0eba817fef76c82772
sougata das <sougata19582001@gmail.com>
2023-01-01 00:31:42 +0530

    changed the api, layout. added setting to ask user for location

--- app.js
+++ app.js
@@ -1,61 +1,86 @@
-const cloud_pct = document.querySelector('.cloud-pct')
-const feels_like = document.querySelector('.feel-like-temp')
-const humidity = document.querySelector('.humidity')
-const max_temp = document.querySelector('.max-temp')
-const min_temp = document.querySelector('.min-temp')
-const temp = document.querySelector('.temperatue')
-const wind_degrees = document.querySelector('.wind-degree')
-const wind_speed = document.querySelector('.wind-speed')
+const cityName = "noida"
 const showDelhi = document.querySelector('.show-delhi')
 const showKolkata = document.querySelector('.show-kolkata')
 const showBangalore = document.querySelector('.show-bangalore')
 const cityNameElement = document.querySelectorAll('.city-name')
 const searchCityBtn = document.querySelector('#search-city')
-
+const KELVIN = 273.15
 
 
 showDelhi.addEventListener('click', function(event){
     event.preventDefault()
-    getWeather('delhi'); 
+    getGeoCode("delhi")
 })
 showKolkata.addEventListener('click', function(event){
     event.preventDefault()
-    getWeather('kolkata');
+    getGeoCode("kolkata")
 })
 showBangalore.addEventListener('click', function(event){
     event.preventDefault()
-    getWeather("bangalore");
+    getGeoCode("bangalore")
 })
-
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "61c3215823msh1ce21b5e6bce66ap117f52jsn985a699a8370",
-    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
-  },
-};
-
-function getWeather(city){
-  fetch(`https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`,options)
-  .then((response) => response.json())
-  .then((response) => {
-    cityNameElement[0].innerHTML = city;
-    cityNameElement[1].innerHTML = city;
-    cloud_pct.innerHTML = `${response.cloud_pct} `;
-    feels_like.innerHTML = `${response.feels_like} \u2103`;
-    humidity.innerHTML = `${response.humidity} `;
-    max_temp.innerHTML = `${response.max_temp} \u2103`;
-    min_temp.innerHTML = `${response.min_temp} \u2103`;
-    temp.innerHTML = `${response.temp} \u2103`;
-    wind_degrees.innerHTML = `${response.wind_degrees} \u00B0`;
-    wind_speed.innerHTML = `${response.wind_speed} Km/hr`;
-    console.log(response);
-  })
-  .catch((err) => console.error(err));
-}
 
 
 searchCityBtn.addEventListener('click', function(event){
   event.preventDefault()
-  getWeather(document.querySelector('#city-input').value);
+  getGeoCode(document.querySelector('#city-input').value);
 })
+
+
+/* OPEN WEATHER API */
+function getWeather(lat, lon){
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=b66275312722953c97e7418fd1b405c4`)
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response)
+    const min_temp = response.main.temp_min - KELVIN
+    const max_temp = response.main.temp_max - KELVIN
+    const feels_like = response.main.feels_like - KELVIN
+    const humidity = response.main.humidity
+    const visibility = response.visibility / 1000
+    const weather = response.weather[0].description
+    const weatherIcon = response.weather[0].icon
+    const cityName = response.name
+    const countryCode = response.sys.country
+    document.querySelector('.city-name').textContent = `${cityName}, ${countryCode}`
+    document.querySelector('.card-img-top').src = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`
+    document.querySelector('.weather-description').textContent = `${weather}`
+    document.querySelector('.temperature').textContent = `${Math.ceil(feels_like)}\u2103`
+    document.querySelector('.max-temp').textContent = `${Math.ceil(max_temp)} \u2103`
+    document.querySelector('.min-temp').textContent = `${Math.ceil(min_temp)} \u2103`
+    document.querySelector('.humidity').textContent = `${humidity}%`
+    document.querySelector('.visibility').textContent = `${visibility}km`    
+  })
+}
+
+function getGeoCode(city){
+  fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=b66275312722953c97e7418fd1b405c4`)
+  .then((response) => response.json())
+  .then((response) =>{
+    console.log(response)
+    const lat = response[0].lat
+    const lon = response[0].lon
+    getWeather(lat, lon)
+  })
+  .catch((err) => console.error(err))
+}
+
+getGeoCode(cityName)
+
+
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+  const crd = pos.coords;
+  getWeather(crd.latitude, crd.longitude)
+}
+
+function error(err) {
+  alert(`ERROR(${err.code}): ${err.message}`);
+}
+
+navigator.geolocation.getCurrentPosition(success, error, options);
